a273a175c646a3c4c9c0dadfa2b76cab23205900
umairayub79 <umairayub79@gmail.com>
2019-11-06 10:54:01 +0500

    Implemented Save Wallpaper

---
+++ app/src/main/java/com/wafihasan/wafers/Util.java
@@ -0,0 +1,18 @@
+package com.wafihasan.wafers;
+
+import android.content.Context;
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
+
+public class Util {
+
+
+    public static boolean isNetworkAvailable(Context context) {
+        ConnectivityManager connectivityManager
+                = (ConnectivityManager) context.getSystemService(Context.CONNECTIVITY_SERVICE);
+        NetworkInfo activeNetworkInfo = connectivityManager.getActiveNetworkInfo();
+        return activeNetworkInfo != null && activeNetworkInfo.isConnected();
+    }
+
+
+}
